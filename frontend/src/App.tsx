c55c1276f2fa8bd0702c1e453fe0b8556684ce89
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-08-22 11:02:41 +0700

    feat: implement App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
-import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
+import { DoctorListPage } from "./pages/DoctorListPage";
 
 const RootLayout = () => {
   return (
@@ -12,33 +13,24 @@ const RootLayout = () => {
   );
 };
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RootLayout />,
-    children: [
-      {
-        path: "/",
-        element: (
-          <div className="py-12 text-center text-gray-500 font-medium">
-            My Appointments Screen (In Progress)
-          </div>
-        ),
-      },
-      {
-        path: "/my-appointments",
-        element: (
-          <div className="py-12 text-center text-gray-500 font-medium">
-            My Appointments Screen (In Progress)
-          </div>
-        ),
-      },
-    ],
-  },
-]);
-
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<DoctorListPage />} />
+          <Route
+            path="my-appointments"
+            element={
+              <div className="py-12 text-center text-gray-500 font-medium">
+                My Appointments Screen (In Progress)
+              </div>
+            }
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/DoctorListPage.tsx
@@ -0,0 +1,9 @@
+export const DoctorListPage = () => {
+  return (
+    <div className="max-w-7xl mx-auto px-4 py-8">
+      <h1 className="text-2xl font-bold text-gray-800 mb-6">
+        Book an Appointment with a Specialist
+      </h1>
+    </div>
+  );
+};
