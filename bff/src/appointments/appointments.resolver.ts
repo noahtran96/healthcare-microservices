9c604d56449e2521f6232de6857f9e84bbbfca73
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-09-12 23:41:03 +0700

    test

--- bff/src/appointments/appointments.resolver.ts
+++ bff/src/appointments/appointments.resolver.ts
@@ -1,7 +1,1 @@
-import { Args, ID, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { Appointment } from './models/appointment.model';
 
-@Resolver(() => Appointment)
-export class AppointmentsResolver {
-  constructor(private readonly httpService: HttpService) {}
-}
