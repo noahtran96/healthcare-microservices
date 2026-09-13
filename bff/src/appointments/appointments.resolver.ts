978758dac8f3d788cfad5570db0eb206fdb9059d
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-09-13 22:52:19 +0700

    feat: implement appointment resolver

--- bff/src/appointments/appointments.resolver.ts
+++ bff/src/appointments/appointments.resolver.ts
@@ -1,1 +1,16 @@
+import { Query, Resolver } from "@nestjs/graphql";
 
+@Resolver(() => Appointment)
+export class AppointmentsResolver {
+  constructor(private readonly httpService: HttpService) {
+
+    @Query(() => [Appointment], {name: "appointments"})
+    async getAppointments(): Promise<Appointment[]> {
+
+      const {data} = await firstValueFrom(
+        this.httpService.get<Appointment[]>("http://127.0.0.1:8080/appointments"),
+      );
+      return data;
+    }
+  }
+}
