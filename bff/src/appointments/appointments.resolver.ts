a8ed4a15aca6b396fa4247729caeb9d6e23d185e
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-09-14 23:57:37 +0700

    test

--- bff/src/appointments/appointments.resolver.ts
+++ bff/src/appointments/appointments.resolver.ts
@@ -7,10 +7,7 @@ export class AppointmentsResolver {
     @Query(() => [Appointment], {name: "appointments"})
     async getAppointments(): Promise<Appointment[]> {
 
-      const {data} = await firstValueFrom(
-        this.httpService.get<Appointment[]>("http://127.0.0.1:8080/appointments"),
-      );
-      return data;
+     
     }
   }
 }
